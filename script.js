4c65bae807d78c08fa1b2d545052201fbe47d843
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-07-25 04:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// your code here
+function updateURL() {
+            const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+            let baseURL = 'https://localhost:8080/';
+            
+            let queryString = '';
+            if (name) {
+                queryString += `name=${encodeURIComponent(name)}`;
+            }
+            if (year) {
+                if (queryString) {
+                    queryString += '&';
+                }
+                queryString += `year=${year}`;
+            }
+            if (queryString) {
+                baseURL += `?${queryString}`;
+            }
+            document.getElementById('url').textContent = baseURL;
+        }
